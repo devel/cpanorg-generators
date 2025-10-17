fcaf36dad659b0ab109c26ea77b3861de915c8b1
Robert <rbrt@pobox.com>
2025-10-17 13:41:12 -0700

    remove some hardcoding

--- bin/perl-label.sh
+++ bin/perl-label.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ~/cpan || exit 1
+# run this from a directory with a CPAN directory in it
 
 # search.cpan.org + perl-sorter and the new TT content system now does
 # all the work previously done by perl-label.pl and the old perl-sorter.pl.
